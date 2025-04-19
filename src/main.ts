d738a8f5874e5a9e7b4a423a30a0c7b2ca94115a
Salvador <josesalvador0102@gmail.com>
2025-04-19 07:25:18 +0100

    up

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  // Configurar CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://trader.mulfex.com'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
