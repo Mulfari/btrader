cd2c2cdbc50bd072fe2c0389a04e298b5b2084a6
Salvador <josesalvador0102@gmail.com>
2025-04-19 20:02:15 +0100

    asd

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
+  const corsOptions: CorsOptions = {
     origin: [
       'http://localhost:3000',
-      'https://trader.mulfex.com'
+      'https://trader.mulfex.com',
+      'https://btrader-production.up.railway.app'
     ],
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
     exposedHeaders: ['Authorization'],
     credentials: true,
-    maxAge: 3600
-  });
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  };
+
+  app.enableCors(corsOptions);
 
   // Configurar prefijo global
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 8000);
+  const port = process.env.PORT ?? 8000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
