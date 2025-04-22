e1e5afee4d2ee6ae1aad7a8f791c4d0ca9c61f22
Salvador <josesalvador0102@gmail.com>
2025-04-22 18:17:04 +0100

    up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TwoFAService } from './twofa.service';
 import { TwoFAController } from './twofa.controller';
 import { AuthGuard } from './auth.guard';
-import { PaymentsModule } from './payments/payments.module';
-import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    PaymentsModule,
-    HealthModule
-  ],
+  imports: [],
   controllers: [AppController, TwoFAController],
   providers: [AppService, TwoFAService, AuthGuard],
 })
